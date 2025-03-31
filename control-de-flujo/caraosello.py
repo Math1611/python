f20a1045510b520e8dc2128e5c05f378120b406f
Math1611 <mathiasrosales490@gmail.com>
2025-03-31 11:18:33 -0300

    if y else

---
+++ control-de-flujo/caraosello.py
@@ -0,0 +1,12 @@
+#mathias rosales 31/03/25
+#al fin se acabo marzo, fiestaaa
+
+import random
+
+num = random.randint(0, 1)
+
+if num > 0.5 :
+    print("cara")
+else:
+    print("sello")
+
---
+++ control-de-flujo/promedio.py
@@ -0,0 +1,18 @@
+##mathias rosales 31/03/25
+#al fin se acabo marzo, fiestaaa
+
+nota1 = 70
+
+if nota1 >=40 :
+    print("pasaste")
+else:
+    print("no pasaste")
+
+
+
+nota = int(input("ingresa tu nota:"))
+
+if nota >= 40 :
+    print("tu pasaste!")
+else :
+    print("No pasaste :c")
